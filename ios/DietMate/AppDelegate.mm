60bb799b992c2287f8219c5c7c891be083a046ba
ofculture8 <ofculture8@gmail.com>
2023-11-02 16:03:19 +0900

    [FEAT]: splashScreen, 앱아이콘 적용

--- ios/DietMate/AppDelegate.mm
+++ ios/DietMate/AppDelegate.mm
@@ -1,6 +1,7 @@
 #import "AppDelegate.h"
 #import <RNKakaoLogins.h>
 #import <React/RCTBundleURLProvider.h>
+#import "RNSplashScreen.h"
 
 
 @implementation AppDelegate
@@ -11,8 +12,11 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+    
+    [super application:application didFinishLaunchingWithOptions:launchOptions];
+    [RNSplashScreen show];  // 추가
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+    return YES; // 수정
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
